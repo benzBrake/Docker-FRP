9a16134c9b475a7f0dd70900d31ef50a5439b901
Ryan Lieu <github-benzbrake@woai.ru>
2019-11-27 21:31:40 +0800

    尝试修复构建失败

--- server/build.sh
+++ server/build.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-if [[ -z ${BUILD_VERSION} ]]; then 
+if [ -z ${BUILD_VERSION} ]; then 
     exit 1
 fi
-BUILD_VERSION=v0.14.1
-_LINK=$(curl --silent https://github.com/fatedier/frp/releases/${BUILD_VERSION} | grep frp_.*_linux_amd64.tar.gz | grep "<a" | awk -F'"' '{print $2}')
-if [[ -z ${_LINK} ]]; then
+_LINK=$(curl --silent https://github.com/fatedier/frp/releases/${BUILD_VERSION} | grep "frp_.*_linux_amd64.tar.gz" | grep "<a" | awk -F'"' '{print $2}')
+if [ -z ${_LINK} ]; then
     # 编译
-    mkdir -pv /go/src/github.com/fatedier
+    go get github.com/tools/godep
     cd /go/src/github.com/fatedier
     git clone https://github.com/fatedier/frp frp
     cd /go/src/github.com/fatedier/frp
@@ -15,12 +14,12 @@ if [[ -z ${_LINK} ]]; then
     chmod +x /go/src/github.com/fatedier/frp/bin/frp
 else
     # 下载二进制
-    go get github.com/tools/godep
+    cd /tmp
     curl -sSL https://github.com${_LINK} -o frp.tar.gz
-    tar xvf frp.tar.gz
+    tar zxvf frp.tar.gz
     rm -f frp.tar.gz
     mv frp* frp
     chmod +x /tmp/frp/frps
-    mkdir /go/src/github.com/fatedier/frp/bin/
+    mkdir -pv /go/src/github.com/fatedier/frp/bin/
     cp /tmp/frp/frps /go/src/github.com/fatedier/frp/bin/
 fi
