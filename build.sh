db0abe2f8c26544b627154d4c295050b23cd22ed
Ryan Lieu <github-benzBrake@woai.ru>
2021-03-04 20:41:03 +0800

    feat: 修改 arch 检测脚本

--- build.sh
+++ build.sh
@@ -7,8 +7,17 @@ case "$(uname -m)" in
 i386) architecture="386" ;;
 i686) architecture="386" ;;
 x86_64) architecture="amd64" ;;
-arm) dpkg --print-architecture | grep -q "arm64" && architecture="arm64" || architecture="arm" ;;
+aarch64) architecture="arm64" ;;
+arm*)
+    if [ -z "$(echo ${architecture} | grep arm64)" ]; then
+        architecture="arm"
+
+    else
+        architecture="arm64"
+    fi
+    ;;
 esac
+[ -z ${architecture} ] && exit 1
 frp_version=$(curl -H 'Cache-Control: no-cache' -s https://api.github.com/repos/fatedier/frp/releases/latest | grep 'tag_name' | awk -F '[:,"v]' '{print $6}')
 frp_download_url="https://github.com/fatedier/frp/releases/download/v${frp_version}/frp_${frp_version}_linux_${architecture}.tar.gz"
 curl -sSL "${frp_download_url}" -o /frp.tar.gz
